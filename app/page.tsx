e1208c88e28ea48ca880715a6aaf50c88867dff0
rons9n <rons9n@gmail.com>
2023-09-11 14:22:06 -0500

    Button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>GenAI</h1>;
+  return <Button variant="destructive">Button</Button>;
 }
