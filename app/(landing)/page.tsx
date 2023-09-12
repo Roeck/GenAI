912d059f744be5b919b73189151f99a97a31259a
rons9n <rons9n@gmail.com>
2023-09-12 14:51:44 -0500

    Auth setup

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,6 +1,8 @@
 // import { LandingNavbar } from "@/components/landing-navbar";
 // import { LandingHero } from "@/components/landing-hero";
 // import { LandingContent } from "@/components/landing-content";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const LandingPage = () => {
   return (
@@ -8,6 +10,17 @@ const LandingPage = () => {
       {/* <LandingNavbar />
       <LandingHero />
       <LandingContent /> */}
+      <div>
+        Landing Page
+        <div>
+          <Link href="/sign-in">
+            <Button>Sign In</Button>
+          </Link>
+          <Link href="/sign-up">
+            <Button>Sign Up</Button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
