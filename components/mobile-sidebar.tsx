a637cd1ec6f06c747a7e9aeab42cbfb76a065cec
rons9n <rons9n@gmail.com>
2023-09-19 15:41:14 -0500

    Sheet component

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Menu } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Sidebar } from "@/components/sidebar";
 
 export const MobileSidebar = ({
@@ -23,5 +24,16 @@ export const MobileSidebar = ({
     return null;
   }
 
-  return <>Sheet</>;
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+        <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
+      </SheetContent>
+    </Sheet>
+  );
 };
