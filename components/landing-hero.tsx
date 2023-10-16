f566212283eaf0adda70214992a63b941f459c6e
rons9n <rons9n@gmail.com>
2023-10-15 21:54:46 -0500

    Landing session update

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -10,17 +10,20 @@ export const LandingHero = () => {
   const { isSignedIn } = useAuth();
 
   return (
-    <div className="text-white font-bold py-36 text-center space-y-5">
+    <div className="text-white font-bold py-36 text-center">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>The Best AI Tool for</h1>
-        <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600">
+        <h1>Top Single-Source Generator for</h1>
+        <div className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-900">
           <TypewriterComponent
             options={{
               strings: [
-                "Chatbot.",
-                "Photo Generation.",
-                "Blog Writing.",
-                "Mail Writing.",
+                "Chatbots,",
+                "Images,",
+                "Music,",
+                "Videos,",
+                "Blog Writing,",
+                "Mail Writing...",
+                "...and Code!",
               ],
               autoStart: true,
               loop: true,
@@ -28,9 +31,7 @@ export const LandingHero = () => {
           />
         </div>
       </div>
-      <div className="text-sm md:text-xl font-light text-zinc-400">
-        Create content using AI 10x faster.
-      </div>
+      <br></br>
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
           <Button
@@ -40,9 +41,6 @@ export const LandingHero = () => {
             Start Generating For Free
           </Button>
         </Link>
-      </div>
-      <div className="text-zinc-400 text-xs md:text-sm font-normal">
-        No credit card required.
       </div>
     </div>
   );
